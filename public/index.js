1dbd4782406ece1a8c79a886df7557f05ff2ba6d
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-24 15:55:14 -0500

    feat: enhance image upload interface with drag-and-drop support and new SVG icon

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
-console.log("PixScale");
+const input = document.getElementById('input-file');
+const counter = document.getElementById('counter');
+
+input.addEventListener('change', uploadImages);
+
+function uploadImages(){
+  if (input.files.length > 0) {
+    counter.innerHTML = `Images uploaded: ${input.files.length}`;
+  }
+}
